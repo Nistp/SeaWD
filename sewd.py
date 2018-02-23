3984f1238b2f8c0745fbd9406b3a1f02ee46bf75
myermakov <proger1503@gmail.com>
2018-02-22 20:06:19 -0500

    create project folder relative to root dir

--- sewd.py
+++ sewd.py
@@ -6,23 +6,32 @@ import os, sys  # for creating folders and files
 import lame
 
 parser = argparse.ArgumentParser('sewd')
-parser.add_argument('action', help="make / push / config") 
-parser.add_argument('target', help="project folder name") 
+parser.add_argument('action', help='make / push / config') 
+parser.add_argument('target', help='project folder name') 
 
 
-def maker(projectname):
-    print(f"making.. {projectname}")
-    #os.chdir("path")  # set the path to the article folder
-    #if not os.path.exists(projectname): # check that folder doesn't exist yet
-    #    os.mkdir(projectname) # create a folder
-    #os.chdir(projectname) # cd in it/do we need an exception for this?
-    #meta = open("meta.yaml", r+) # what goes in here?
-    #post = open("post.md", r+) # what goes in here?
+def maker(project_name):
+    print(f'making.. {project_name}')
+    print(f'__file__ is {__file__}')
+    print(f'path to file is {os.path.abspath(__file__)}')
+    print('now how your path is different from my path')
+    root_folder = os.path.dirname((os.path.abspath(__file__))) # can be made grapes again
+    os.chdir(root_folder)   
+    print('TODO: make amd use all_projects foolder path from yaml config')
+
+    if os.path.exists(project_name):
+        print('project exists, moving there..')
+    else: 
+        os.mkdir(project_name)
+    os.chdir(project_name) # these might need to be relative, with os.path.join()
+
+    with open(f'{project_name}.md', 'w') as f:
+       f.write('# test') 
+    
 
 
-
-def pusher(projectname):
-    print('pushing.. {projectname}')
+def pusher(project_name):
+    print('pushing.. {project_name}')
     pass
 
 
