aae90b343f59a67bdcacf159e2e58d16583bbfe9
Nistp <31832285+Nistp@users.noreply.github.com>
2018-03-04 15:37:03 -0600

    Add files via upload
    TwitterBot module, which later should be extended to multiple platforms (i.e. FB, Linkedin, etc), since once we master OAuth they all should be easy and be almost practically similar

---
+++ twitterthing.py
@@ -0,0 +1,48 @@
+#Simple twitter bot in Python
+
+import time, sys, requests, requests_oauthlib
+from requests_oauthlib import OAuth1Session
+
+CONSUMER_KEY = "..."
+CONSUMER_SECRET = "..."
+ACCESS_KEY = "..."
+ACCESS_SECRET = "..."
+
+Request_token_URL = 'https://api.twitter.com/oauth/request_token'
+Authorize_URL ='https://api.twitter.com/oauth/authorize'
+Access_token_URL =  'https://api.twitter.com/oauth/access_token' 
+
+twitterbot = OAuth1Session(CONSUMER_KEY, client_secret=CONSUMER_SECRET)
+fetch_response = twitterbot.fetch_request_token(Request_token_URL)
+
+rok = fetch_response.get('oauth_token')
+ros = fetch_response.get('oauth_token_secret')
+
+#gets messed up here with an 89 error code
+twitterbot_response = twitterbot.parse_authorization_response(Authorize_URL)
+#needed or not? 
+verifier = twitterbot_response.get('oauth_verifier')
+#needed or not?
+access_token_url = 'https://api.twitter.com/oauth/access_token'
+
+twitterbot = OAuth1Session(CONSUMER_KEY,
+                         client_secret=CONSUMER_SECRET,
+                         resource_owner_key=rok,
+                          resource_owner_secret=ros,
+                         verifier=verifier)
+				
+				
+twitterbot_tokens = twitterbot.fetch_access_token(access_token_url)
+
+rok = fetch_response.get('oauth_token')
+ros = fetch_response.get('oauth_token_secret')
+
+twitterbot = OAuth1Session(CONSUMER_KEY,
+                          client_secret=CONSUMER_SECRET,
+                          resource_owner_key=rok,
+                          resource_owner_secret=ros)
+						  
+
+status_url = 'http://api.twitter.com/1.1/statuses/update.json'
+new_status = {'status':  'Pythonic Test Tweet!'}
+twitterbot.post(status_url, data=new_status)
